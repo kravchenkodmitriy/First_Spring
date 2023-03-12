beb6daeee51e125be3ec185d20928affe090091a
Dmitriy <kravchenkodmitriy38@gmail.com>
2023-03-12 22:10:54 +0300

    Initial commit

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.text.DateFormat;
+import java.util.Date;
 
 @Controller
 public class MainController {
 
+    private DateFormat dateFormat;
     @GetMapping("/greeting") //http://localhost:8080/greeting
     public String greeting(@RequestParam(required = false, defaultValue = "World") String name, Model model) {
+        Date date = new Date();
+        dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
         model.addAttribute("name", name);
+        model.addAttribute("date", dateFormat.format(date));
         return "greeting";
     }
 }
